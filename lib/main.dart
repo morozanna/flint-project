3b21d8ab6641b71d2088574cc48cadf36df32c37
morozanna <a.moroz201648@gmail.com>
2021-02-18 12:59:50 +0100

    changed multi provider positon in wigdet tree

--- lib/main.dart
+++ lib/main.dart
@@ -21,29 +21,32 @@ class MyApp extends StatelessWidget {
           return Container(); //TODO: add error screen
         }
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            title: 'Wishes App',
-            theme: ThemeData(
-              // This is the theme of your application.
-              //
-              // Try running your application with "flutter run". You'll see the
-              // application has a blue toolbar. Then, without quitting the app, try
-              // changing the primarySwatch below to Colors.green and then invoke
-              // "hot reload" (press "r" in the console where you ran "flutter run",
-              // or simply save your changes to "hot reload" in a Flutter IDE).
-              // Notice that the counter didn't reset back to zero; the application
-              // is not restarted.
-              primarySwatch: Colors.deepOrange,
-              // This makes the visual density adapt to the platform that you run
-              // the app on. For desktop platforms, the controls will be smaller and
-              // closer together (more dense) than on mobile platforms.
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-            home: MultiProvider(providers: [
+          return MultiProvider(
+            providers: [
               ChangeNotifierProvider(
                 create: (context) => WishService(),
               )
-            ], child: WishesScreen()),
+            ],
+            child: MaterialApp(
+              title: 'Wishes App',
+              theme: ThemeData(
+                // This is the theme of your application.
+                //
+                // Try running your application with "flutter run". You'll see the
+                // application has a blue toolbar. Then, without quitting the app, try
+                // changing the primarySwatch below to Colors.green and then invoke
+                // "hot reload" (press "r" in the console where you ran "flutter run",
+                // or simply save your changes to "hot reload" in a Flutter IDE).
+                // Notice that the counter didn't reset back to zero; the application
+                // is not restarted.
+                primarySwatch: Colors.deepOrange,
+                // This makes the visual density adapt to the platform that you run
+                // the app on. For desktop platforms, the controls will be smaller and
+                // closer together (more dense) than on mobile platforms.
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: WishesScreen(),
+            ),
           );
         }
         return Container(); //TODO: add loading screen
