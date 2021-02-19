0ce9f659ab4532c3823b280dc81482acd6034eaf
morozanna <a.moroz201648@gmail.com>
2021-02-19 16:30:49 +0100

    fixed imports

--- lib/utils/services/wishService.dart
+++ lib/utils/services/wishService.dart
@@ -1,18 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flint_project/models/wishModel.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class WishService extends ChangeNotifier {
+  String collectionName = 'wishes';
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Future<Wish> getWish(String id) async {
-    var snap = await _db.collection('wishes').doc(id).get();
+    var snap = await _db.collection(collectionName).doc(id).get();
     return Wish.fromMap(snap.data());
   }
 
   Future<List<Wish>> getWishes() async {
     final List<Wish> list = [];
-    await _db.collection('wishes').get().then((querySnapshot) {
+    await _db.collection(collectionName).get().then((querySnapshot) {
       querySnapshot.docs.forEach((element) {
         var wish = Wish.fromMap(element.data());
         list.add(wish);
@@ -23,11 +24,13 @@ class WishService extends ChangeNotifier {
   }
 
   Future addWish(Wish wish) async {
-    var result =
-        await _db.collection('wishes').add(wish.toMap()).then((value) async {
+    var result = await _db
+        .collection(collectionName)
+        .add(wish.toMap())
+        .then((value) async {
       wish.id = value.id;
       var _result = await _db
-          .collection('wishes')
+          .collection(collectionName)
           .doc(value.id)
           .set({"id": value.id}, SetOptions(merge: true));
       return _result;
@@ -38,7 +41,7 @@ class WishService extends ChangeNotifier {
 
   Future editWish(Wish editedWish) async {
     var result = await _db
-        .collection('wishes')
+        .collection(collectionName)
         .doc(editedWish.id)
         .set({"content": editedWish.content}, SetOptions(merge: true));
     notifyListeners();
@@ -47,7 +50,7 @@ class WishService extends ChangeNotifier {
   }
 
   Future deleteWish(String id) async {
-    var result = await _db.collection('wishes').doc(id).delete();
+    var result = await _db.collection(collectionName).doc(id).delete();
     notifyListeners();
 
     return result;
