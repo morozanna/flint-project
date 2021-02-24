1d8235ae400be81be00046423bc68cf8c4afcf56
morozanna <a.moroz201648@gmail.com>
2021-02-24 14:16:44 +0100

    addded authentication service

---
+++ lib/utils/auth/authService.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authentication {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> login(String email, String password) async {
+    User user;
+    String error;
+    try {
+      var result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = result.user;
+    } catch (e) {
+      if (e.code == 'user-not-found') {
+        error = 'Error: No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        error = 'Error: Wrong password provided for that user.';
+      } else
+        error = "Error: " + e.code;
+    }
+    if (error != null) {
+      return error;
+    }
+    return user.uid;
+  }
+
+  Future<void> logout() async {
+    return await _auth.signOut();
+  }
+
+  User getUser() {
+    return _auth.currentUser;
+  }
+}
