a2a4a8a06011426b12f16fa8a9ccbf34458f943e
morozanna <a.moroz201648@gmail.com>
2021-03-01 11:40:34 +0100

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -38,21 +38,16 @@ class MyApp extends StatelessWidget {
             child: MaterialApp(
               title: 'Wishes App',
               theme: ThemeData(
-                // This is the theme of your application.
-                //
-                // Try running your application with "flutter run". You'll see the
-                // application has a blue toolbar. Then, without quitting the app, try
-                // changing the primarySwatch below to Colors.green and then invoke
-                // "hot reload" (press "r" in the console where you ran "flutter run",
-                // or simply save your changes to "hot reload" in a Flutter IDE).
-                // Notice that the counter didn't reset back to zero; the application
-                // is not restarted.
-                primarySwatch: Colors.deepOrange,
-                // This makes the visual density adapt to the platform that you run
-                // the app on. For desktop platforms, the controls will be smaller and
-                // closer together (more dense) than on mobile platforms.
-                visualDensity: VisualDensity.adaptivePlatformDensity,
-              ),
+                  brightness: Brightness.dark,
+                  primarySwatch: Colors.deepPurple,
+                  visualDensity: VisualDensity.adaptivePlatformDensity,
+                  textTheme: TextTheme(
+                    headline1:
+                        TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                    bodyText1: TextStyle(
+                      fontSize: 20,
+                    ),
+                  )),
               home: WishesScreen(),
             ),
           );
